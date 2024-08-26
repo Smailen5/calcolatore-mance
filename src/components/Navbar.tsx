5a4b496ae052d7e9d48a3420c3a5435b06f2d09b
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-08-26 18:31:41 +0200

    feat: :sparkles: aggiunge componente
    crea Navbar con un minimo di stile

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <nav className="flex justify-between bg-secondary p-4">
+      <h1>Calcolatore Mance</h1>
+      {/* aggiungi qui il componente menu per navigare fra le pagine */}
+      <div>menu</div>
+    </nav>
+  );
+};
+
+export default Navbar;
