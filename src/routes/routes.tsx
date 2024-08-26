95318c0215e48d41786d97431fc265916a7891ec
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-08-26 19:43:31 +0200

    docs: :truck: aggiorna routes
    aggiunge nuova route

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 import Error from "../pages/ErrorPage";
 import Home from "../pages/HomePage";
+import Params from "../pages/ParamsPage";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+  },
+  {
+    path: "/parametri",
+    element: <Params />,
   },
   {
     path: "*",
