3450bd3f68fef060c62ea100dd0d6065c34202e7
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-08-26 18:46:21 +0200

    feat: :sparkles: aggiunge componente
    crea Menu con bozza di codice

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,11 @@
+import Menu from "./Menu";
+
 const Navbar = () => {
   return (
     <nav className="flex justify-between bg-secondary p-4">
       <h1>Calcolatore Mance</h1>
       {/* aggiungi qui il componente menu per navigare fra le pagine */}
-      <div>menu</div>
+      <Menu />
     </nav>
   );
 };
