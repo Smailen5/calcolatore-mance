99a87ff5e34d1d3f1afec05ac9eda4f4bf815148
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-08-27 17:29:07 +0200

    refactor: :art: importa componente
    importa UserGrid in ParamasPage

--- src/pages/ParamsPage.tsx
+++ src/pages/ParamsPage.tsx
@@ -1,6 +1,7 @@
 import Footer from "@/components/Footer";
 import FormAddUser from "@/components/FormAddUser";
 import Navbar from "@/components/Navbar";
+import UserGrid from "@/components/UserGrid";
 
 const ParamsPage = () => {
   return (
@@ -8,6 +9,7 @@ const ParamsPage = () => {
       <Navbar />
       <main className="flex h-[86vh] flex-col justify-center gap-8 px-4">
         <FormAddUser />
+        <UserGrid />
       </main>
       <Footer />
     </>
