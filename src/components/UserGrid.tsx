61f277e426253a1e87d3800cd110887bb715a6a7
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-08-27 18:35:16 +0200

    refactor: :construction: crea componente
    crea il componente UserGrid ed utilizza SingleUser passando correttamente i dati

--- src/components/UserGrid.tsx
+++ src/components/UserGrid.tsx
@@ -1,9 +1,22 @@
 import { useGlobalContext } from "@/utils/contex";
+import SingleUser from "./SingleUser";
 
 const UserGrid = () => {
   const { dataUser } = useGlobalContext();
-  console.log(dataUser)
-  return <div>UserGrid</div>;
+
+  // se non ci sono user visualizza un messaggio
+  if (!dataUser || dataUser.length === 0) {
+    return <p className="text-center">Non hai ancora aggiunto dipendenti</p>;
+  }
+
+  return (
+    <section className="grid grid-cols-2 gap-2">
+      {/* se dataUser ha dei dati visualizza gli user */}
+      {dataUser &&
+        dataUser.length > 0 &&
+        dataUser.map((user) => <SingleUser key={user.name} {...user} />)}
+    </section>
+  );
 };
 
 export default UserGrid;
