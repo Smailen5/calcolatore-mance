d5db357bd8d245ef4002477cf960627509209ff5
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-08-27 19:47:13 +0200

    refactor: :construction: aggiorna componente
    importa setDataUser per modificare lo stato globale e localStorage, aggiorna il codice di conseguenza, aggiunge messaggio per l'utente eliminato

--- src/components/UserGrid.tsx
+++ src/components/UserGrid.tsx
@@ -1,21 +1,47 @@
 import { useGlobalContext } from "@/utils/contex";
 import SingleUser from "./SingleUser";
+import { useState } from "react";
 
 const UserGrid = () => {
-  const { dataUser } = useGlobalContext();
+  const { dataUser, setDataUser } = useGlobalContext();
+  const [messageUser, setMessageUser] = useState("Ecco tutti gli utenti:");
 
   // se non ci sono user visualizza un messaggio
   if (!dataUser || dataUser.length === 0) {
     return <p className="text-center">Non hai ancora aggiunto dipendenti</p>;
   }
 
+  // rimuove user selezionato e aggiorna i dati
+  const removeUser = (id: string, name: string) => {
+    const newUserList = dataUser.filter((user) => user.id !== id);
+    setDataUser(newUserList);
+    // aggiungi un messaggio di conferma con setTimeout
+    setMessageUser(`Utente eliminato: ${name} 👋`);
+    console.log(name)
+    const timer = setTimeout(() => {
+      setMessageUser("Ecco tutti gli utenti:");
+    }, 1500);
+    return () => clearTimeout(timer);
+  };
+
   return (
-    <section className="grid grid-cols-2 gap-2">
-      {/* se dataUser ha dei dati visualizza gli user */}
-      {dataUser &&
-        dataUser.length > 0 &&
-        dataUser.map((user) => <SingleUser key={user.name} {...user} />)}
-    </section>
+    <>
+      {/* messaggio dinamico che cambia se viene eliminato un user */}
+      <p className="text-center">{messageUser}</p>
+      <section className="grid grid-cols-2 gap-2">
+        {/* se dataUser ha dei dati visualizza gli user */}
+        {dataUser &&
+          dataUser.length > 0 &&
+          dataUser.map((utente) => (
+            <SingleUser
+              // attenzione a dare un id univoco o removeUser rimuovera tutti gli utenti con lo stesso nome
+              key={utente.id}
+              {...utente}
+              remove={() => removeUser(utente.id, utente.name)}
+            />
+          ))}
+      </section>
+    </>
   );
 };
 
