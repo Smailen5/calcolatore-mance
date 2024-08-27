211be82ef79641d8dab18224c63a28b238fe4beb
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-08-27 19:48:41 +0200

    feat: :sparkles: aggiunge funzionalita
    aggiunge la funzione per rimuovere il singolo utente

--- src/components/SingleUser.tsx
+++ src/components/SingleUser.tsx
@@ -8,27 +8,32 @@ interface SingleUserProps {
   name: string;
   anniServizio: string;
   contratto: string;
+  remove: () => void;
 }
 
 const SingleUser: React.FC<SingleUserProps> = ({
   name,
   anniServizio,
   contratto,
+  remove,
 }) => {
   return (
-    <article className="relative rounded-md border p-4 text-sm shadow-md">
+    <>
+      <article className="relative rounded-md border p-4 text-sm shadow-md">
         {/* bottone per eliminare singolo user */}
-      <Button
-        size={"sm"}
-        variant={"destructive"}
-        className="absolute right-0 top-0"
-      >
-        X
-      </Button>
-      <h2 className="capitalize">Nome: {name}</h2>
-      <p>Anni di lavoro: {anniServizio}</p>
-      <p>Contratto: {contratto}</p>
-    </article>
+        <Button
+          size={"sm"}
+          variant={"destructive"}
+          className="absolute right-0 top-0"
+          onClick={remove}
+        >
+          X
+        </Button>
+        <h2 className="capitalize">Nome: {name}</h2>
+        <p>Anni di lavoro: {anniServizio}</p>
+        <p>Contratto: {contratto}</p>
+      </article>
+    </>
   );
 };
 
