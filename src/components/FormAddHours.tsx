beeae0984be5e5a47e144c38aac095e38436d227
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-08-29 01:14:15 +0200

    refactor: :poop: aggiunge formik
    aggiunge formik ma forse non serve, posso fare una piccola validazione di numero massimo e numero minimo e lasciare le cose semplici per gestire lo stato

--- src/components/FormAddHours.tsx
+++ src/components/FormAddHours.tsx
@@ -1,5 +1,8 @@
 import { Button } from "./ui/button";
 import { useGlobalContext } from "../utils/contex";
+import { useEffect, useState } from "react";
+import { Field, Formik } from "formik";
+import { number, object } from "yup";
 
 type FormAddHoursProps = {
   nameUser: string;
@@ -7,30 +10,61 @@ type FormAddHoursProps = {
   isVisible: boolean;
 };
 
+const initialValues = {
+  oreLavorate: "",
+};
+
+const validationSchema = object({
+  oreLavorate: number().required(),
+});
+
 const FormAddHours: React.FC<FormAddHoursProps> = ({
   nameUser,
   handleAddHours,
   isVisible,
 }) => {
-  const { hours, setHours } = useGlobalContext();
+  // stati e useGlobalContext
+  const { hours, setHours, setDataUser } = useGlobalContext();
+  const [saveHour, setSaveHour] = useState(false);
+
+  // gestisce il messaggio se le ore sono state salvate
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setSaveHour(true);
+    }, 1000);
+    return clearTimeout(timer);
+  }, [hours, saveHour]);
+
   return (
     <>
       {/* input per aggiungere le ore solo se ce un utente selezionato */}
       {isVisible && (
-        <div className="flex flex-col gap-6 rounded-md border p-4 shadow-md">
-          <h3>
-            Utente selezionato:{" "}
-            <span className="font-bold capitalize">{nameUser}</span>
-          </h3>
-          <input
-            type="number"
-            value={hours === 0 ? "" : hours}
-            onChange={(e) => setHours(Number(e.target.value))}
-            placeholder="Inserisci le ore lavorate"
-            className="w-full rounded-md border border-slate-300 p-2"
-          />
-          <Button onClick={() => handleAddHours()}>Aggiungi ore</Button>
-        </div>
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={(values) => {
+            // lo stato viene comunque aggiornato correttamente anche senza Formik
+          }}
+        >
+          <div className="flex flex-col gap-6 rounded-md border p-4 shadow-md">
+            <h3>
+              Utente selezionato:{" "}
+              <span className="font-bold capitalize">{nameUser}</span>
+            </h3>
+            <Field
+              type="number"
+              name="oreLavorate"
+              value={hours === 0 ? "" : hours}
+              onChange={(e) => setHours(Number(e.target.value))}
+              placeholder="Inserisci le ore lavorate"
+              className="w-full rounded-md border border-slate-300 p-2"
+            />
+            <Button onClick={() => handleAddHours()}>Aggiungi ore</Button>
+            {saveHour && (
+              <p className="text-green-400">Ore aggiunte correttamente</p>
+            )}
+          </div>
+        </Formik>
       )}
     </>
   );
