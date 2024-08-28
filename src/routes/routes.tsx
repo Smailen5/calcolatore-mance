ef28b6bf358de6f9ffb7820bd6e7f217533df9a8
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-08-28 17:29:17 +0200

    refactor: :truck: aggiunge route
    aggiunge nuova route per il form aggiungi ore

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Error from "../pages/ErrorPage";
 import Home from "../pages/HomePage";
 import Params from "../pages/UtentiPage";
+import HoursPage from "@/pages/HoursPage";
 
 export const router = createBrowserRouter([
   {
@@ -11,6 +12,10 @@ export const router = createBrowserRouter([
   {
     path: "/utenti",
     element: <Params />,
+  },
+  {
+    path: "/aggiungi-ore",
+    element: <HoursPage />,
   },
   {
     path: "*",
