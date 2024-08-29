b2ca78a40665a42b4b820833e4d007a51d7bb71c
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-08-29 21:46:00 +0200

    refactor: :art: importa funzione
    importa funzione nel componente

--- src/pages/HoursPage.tsx
+++ src/pages/HoursPage.tsx
@@ -2,26 +2,30 @@ import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import Page from "@/components/Page";
 import UserSession from "@/components/UserSession";
-import { useEffect } from "react";
+// import { useEffect } from "react";
 import { useGlobalContext } from "@/utils/contex";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
 
 const HoursPage = () => {
-  const { setDataUserHoursSession } = useGlobalContext();
+  const { setDataUserHoursSession, recuperaUtenti } = useGlobalContext();
+  const [isVisible, setIsVisible] = useState(true);
 
-  // aggiorna i dati in sessionStorage quando il sessionStorage cambia
-  useEffect(() => {
-    const storeData = localStorage.getItem("user");
-    if (storeData) {
-      const user = JSON.parse(storeData);
-      setDataUserHoursSession(user);
+  const handleRecuperaUtenti = () => {
+    if (recuperaUtenti) {
+      recuperaUtenti({ data: setDataUserHoursSession });
+      setIsVisible(false);
     }
-  }, [setDataUserHoursSession]);
+  };
 
   return (
     <>
       <Navbar />
       <Page>
         <h2>Seleziona un utente per aggiungere/modificare le ore</h2>
+        {isVisible && (
+          <Button onClick={handleRecuperaUtenti}>recupera utenti</Button>
+        )}
         <UserSession />
       </Page>
       <Footer />
