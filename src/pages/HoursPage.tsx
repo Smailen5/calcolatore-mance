90f2b66684eb8de7fef66413fb6f3d8cf3aee281
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-08-29 23:28:03 +0200

    refactor: :recycle: cambia testo
    cambia testo, corregge import contesto globale

--- src/pages/HoursPage.tsx
+++ src/pages/HoursPage.tsx
@@ -3,11 +3,12 @@ import Navbar from "@/components/Navbar";
 import Page from "@/components/Page";
 import { Button } from "@/components/ui/button";
 import UserSession from "@/components/UserSession";
-import { useGlobalContext } from "@/utils/contex";
+import { useGlobalContext } from "@/utils/context";
 import { useState } from "react";
 
 const HoursPage = () => {
-  const { setDataUserHoursSession, recuperaUtenti, setIsSave } = useGlobalContext();
+  const { setDataUserHoursSession, recuperaUtenti, setIsSave } =
+    useGlobalContext();
   const [isVisible, setIsVisible] = useState(true);
 
   const handleRecuperaUtenti = () => {
@@ -21,10 +22,16 @@ const HoursPage = () => {
     <>
       <Navbar />
       <Page>
-        <h2>Seleziona un utente per aggiungere/modificare le ore</h2>
-        {isVisible && (
-          <Button onClick={handleRecuperaUtenti}>recupera utenti</Button>
-        )}
+        <div className="flex flex-col gap-4 rounded-md border p-4 text-center shadow-md">
+          <h2>Clicca un utente per aggiungere/modificare le ore</h2>
+          <p>
+            Recupera gli utenti solo quando hai aggiunto o rimosso un utente dal
+            elenco
+          </p>
+          {isVisible && (
+            <Button onClick={handleRecuperaUtenti}>recupera utenti</Button>
+          )}
+        </div>
         <UserSession />
       </Page>
       <Footer />
