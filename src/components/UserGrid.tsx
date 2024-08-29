a45895f1918cd6a5f325690a57a409a8d587c351
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-08-29 18:51:00 +0200

    refactor: :fire: rimuove codice
    rimuove codice inutilizzato

--- src/components/UserGrid.tsx
+++ src/components/UserGrid.tsx
@@ -1,26 +1,17 @@
-import { useGlobalContext } from "@/utils/contex";
+import { useGlobalContext, UserFormValues } from "@/utils/contex";
 import { useState } from "react";
-// import FormAddHours from "./FormAddHours";
 import SingleUser from "./SingleUser";
-import { UserFormValues } from "@/utils/contex";
 
 type UserGridProps = {
   noButton?: boolean;
   data?: UserFormValues[];
+  selectable?: boolean;
 };
 
-const UserGrid = ({ noButton, data }: UserGridProps) => {
-  const {
-    // dataUser,
-    setDataUser,
-    setIsSave,
-    // hours,
-    // setHours,
-    // setDataUserHoursSession,
-  } = useGlobalContext();
+const UserGrid = ({ noButton, data, selectable }: UserGridProps) => {
+  const { setDataUser, setIsSave, setSelectedUser, setNameUser } =
+    useGlobalContext();
   const [messageUser, setMessageUser] = useState("Ecco tutti gli utenti:");
-  // const [selectedUser, setSelectedUser] = useState<string>("");
-  // const [nameUser, setNameUser] = useState<string>("");
 
   // se non ci sono user visualizza un messaggio
   if (!data || data.length === 0) {
@@ -42,59 +33,31 @@ const UserGrid = ({ noButton, data }: UserGridProps) => {
   };
 
   // gestisce la selezione del user
-  // const handleSelectUser = (id: string) => {
-  //   const selectedUser = dataUser.find((user) => user.id === id);
-  //   if (selectedUser) {
-  //     setSelectedUser(id);
-  //     setNameUser(selectedUser.name);
-  //   }
-  //   return nameUser;
-  // };
+  const handleSelectUser = (id: string) => {
+    const selectedUser = data.find((user) => user.id === id);
+    console.log(selectedUser);
 
-  // gestisce l'inserimento delle ore
-  // const handleAddHours = () => {
-  //   // Verifica che ci sia un utente selezionato e che l'input ore non sia vuoto
-  //   if (selectedUser && hours) {
-  //     const updatedUserList = dataUser.map((user) => {
-  //       if (user.id === selectedUser) {
-  //         return {
-  //           ...user,
-  //           oreLavorate: hours, // Aggiorna oreLavorate
-  //         };
-  //       }
-  //       return user; // Ritorna gli altri utenti senza modifiche
-  //     });
-  //     // setDataUser(updatedUserList); // Aggiorna lo stato con la nuova lista utenti
-  //     setDataUserHoursSession(updatedUserList); // Aggiorna sessionStorage con gli stessi dati di localStorage
-  //     setIsSave(true); // Segnala che ci sono modifiche da salvare
-  //     setHours(0); // Resetta il valore delle ore
-  //     setNameUser(""); // Resetta il nome dell'utente selezionato
-  //     setSelectedUser(""); // Resetta l'utente selezionato
-  //   }
-  // };
+    if (selectedUser) {
+      setSelectedUser(selectedUser.id);
+      setNameUser(selectedUser.name);
+      console.log("utente selezionato");
+    }
+    // return nameUser;
+  };
 
   return (
     <>
-      {/* compare solo se viene selezionato un utente */}
-      {/* <FormAddHours
-        nameUser={nameUser}
-        handleAddHours={handleAddHours}
-        //  CONTROLLA BENE QUESTO, NON LO CAPISCO BENE
-        isVisible={!!selectedUser}
-      /> */}
-      {/* messaggio dinamico che cambia se viene eliminato un user */}
       <p className="text-center">{messageUser}</p>
       <section className="grid grid-cols-2 gap-2">
-        {/* se dataUser ha dei dati visualizza gli user */}
         {data &&
           data.length > 0 &&
           data.map((utente) => (
             <SingleUser
-              // attenzione a dare un id univoco o removeUser rimuovera tutti gli utenti con lo stesso nome
               key={utente.id}
               {...utente}
               remove={() => removeUser(utente.id, utente.name)}
-              // onSelect={() => handleSelectUser(utente.id)}
+              selectable={selectable}
+              onSelect={() => handleSelectUser(utente.id)}
               noButton={noButton}
             />
           ))}
