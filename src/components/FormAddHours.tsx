d3c214049afdba46402e9945f2c4b635988bab85
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-08-29 21:48:25 +0200

    refactor: :art: crea modale
    crea modale nel componente e cambia struttura componente per gestirlo

--- src/components/FormAddHours.tsx
+++ src/components/FormAddHours.tsx
@@ -1,6 +1,7 @@
 import { Button } from "./ui/button";
 import { useGlobalContext } from "../utils/contex";
 import { useEffect, useState } from "react";
+import { Cross1Icon } from "@radix-ui/react-icons";
 
 type FormAddHoursProps = {
   nameUser: string;
@@ -13,7 +14,7 @@ const FormAddHours: React.FC<FormAddHoursProps> = ({
   handleAddHours,
   isVisible,
 }) => {
-  const { hours, setHours } = useGlobalContext();
+  const { hours, setHours, setNameUser, setSelectedUser } = useGlobalContext();
   const [saveHour, setSaveHour] = useState(false);
 
   // gestisce il messaggio se le ore sono state salvate
@@ -29,33 +30,52 @@ const FormAddHours: React.FC<FormAddHoursProps> = ({
   const handleSaveHours = () => {
     handleAddHours();
     setSaveHour(true);
+    const timer = setTimeout(() => {
+      closeModal();
+    }, 1000);
+    return () => clearTimeout(timer);
+  };
+
+  const closeModal = () => {
+    setHours(0); // Resetta il valore delle ore
+    setNameUser(""); // Resetta il nome dell'utente selezionato
+    setSelectedUser(""); // Resetta l'utente selezionato
   };
 
   return (
     <>
-      {/* input per aggiungere le ore solo se ce un utente selezionato */}
       {isVisible && (
-        <div className="flex flex-col gap-6 rounded-md border p-4 shadow-md">
-          <h3>
-            Utente selezionato:{" "}
-            <span className="font-bold capitalize">{nameUser}</span>
-          </h3>
-          <input
-            type="number"
-            name="oreLavorate"
-            value={hours === 0 ? "" : hours}
-            onChange={(e) => setHours(Number(e.target.value))}
-            placeholder="Inserisci le ore lavorate"
-            className="w-full rounded-md border border-slate-300 p-2"
-          />
-          <Button onClick={handleSaveHours}>Aggiungi ore</Button>
-        </div>
-      )}
-      {saveHour && (
-        <div className="rounded-md border p-4 shadow-md">
-          <p className="text-center text-green-400">
-            Ore aggiunte correttamente
-          </p>
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-60">
+          <div className="relative mx-auto max-w-md rounded-lg bg-background p-6">
+            <Button
+              variant={"destructive"}
+              size={"icon"}
+              className="absolute right-2 top-2 size-8 text-background"
+              onClick={closeModal}
+            >
+              <Cross1Icon className="size-4" />
+            </Button>
+            <h3>
+              Utente selezionato:{" "}
+              <span className="font-bold capitalize">{nameUser}</span>
+            </h3>
+            <input
+              type="number"
+              name="oreLavorate"
+              value={hours === 0 ? "" : hours}
+              onChange={(e) => setHours(Number(e.target.value))}
+              placeholder="Inserisci le ore lavorate"
+              className="mt-4 w-full rounded-md border border-slate-300 p-2"
+            />
+            <Button className="mt-4 w-full" onClick={handleSaveHours}>
+              Aggiungi ore
+            </Button>
+            {saveHour && (
+              <p className="mt-4 text-center text-green-400">
+                Ore aggiunte correttamente
+              </p>
+            )}
+          </div>
         </div>
       )}
     </>
