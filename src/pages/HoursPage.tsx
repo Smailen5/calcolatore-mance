779713ab665f601e95c99a9f8547348ed19ad527
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-08-29 17:51:24 +0200

    refactor: :construction: importa contesto
    importa global context per ottenere i dati di dataUserHoursSession e passarli al componente UserGrid

--- src/pages/HoursPage.tsx
+++ src/pages/HoursPage.tsx
@@ -2,14 +2,19 @@ import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import Page from "@/components/Page";
 import UserGrid from "@/components/UserGrid";
+// import UserGrid from "@/components/UserGrid";
+// import UserSession from "@/components/UserSession";
+import { useGlobalContext } from "@/utils/contex";
 
 const HoursPage = () => {
+const {dataUserHoursSession} = useGlobalContext()
   return (
     <>
       <Navbar />
       <Page>
         <h2>Aggiungi/modifica ore utenti</h2>
-        <UserGrid noButton={true} />
+        {/* <UserSession data={dataUser} /> */}
+        <UserGrid data={dataUserHoursSession} />
       </Page>
       <Footer />
     </>
