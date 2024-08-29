8454d60db759b1997360f485fc349a064dc956c7
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-08-29 21:47:09 +0200

    refactor: :lipstick: aggiorna componente
    aggiorna componente con classi per gestire l'apparsa del modale

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,6 +1,6 @@
 const Page = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="flex min-h-[86vh] flex-col items-center justify-center gap-8 px-4 py-8">
+    <main className="flex min-h-[86vh] flex-col items-center justify-center gap-8 px-4 py-8 relative">
       {children}
     </main>
   );
