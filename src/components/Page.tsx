3bbdc79f71d9c832a41502a67185464dab59f13a
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-09-04 21:00:01 +0200

    refactor: :lipstick: rimuove classe
    rimuove classe per dare uno stile coerente a tutte le pagine

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,6 +1,6 @@
 const Page = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="flex min-h-[86vh] flex-col items-center justify-center gap-8 px-4 py-8 relative">
+    <main className="flex min-h-[86vh] flex-col justify-center gap-8 px-4 py-8 relative">
       {children}
     </main>
   );
