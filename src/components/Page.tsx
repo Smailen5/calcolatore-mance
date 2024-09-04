e49536b9b83f89c713ffd871e5ed4b337df0dd4c
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-09-04 21:18:20 +0200

    refactor: :lipstick: modifica dimensioni
    modifica altezza minima della pagina, modifica padding superiore e inferiore

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,6 +1,6 @@
 const Page = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="flex min-h-[86vh] flex-col justify-center gap-8 px-4 py-8 relative">
+    <main className="flex min-h-[100vh] flex-col justify-center gap-8 px-4 pb-8 pt-24">
       {children}
     </main>
   );
