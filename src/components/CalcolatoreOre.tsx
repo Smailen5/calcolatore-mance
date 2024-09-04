b09662b40f00ad341099009601e42d20cf700df9
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-09-04 17:49:12 +0200

    fix: :bug: inverte valori
    inverte i valori per dividere correttamente le mance
    
    fix il valore di mancia per singolo utente e sbagliato, ore viene calcolato correttamente

--- src/components/CalcolatoreOre.tsx
+++ src/components/CalcolatoreOre.tsx
@@ -8,21 +8,27 @@ type calcolatoreOreProps = {
 const CalcolatoreOre:React.FC<calcolatoreOreProps> = ({manciaTotale}) => {
   const { dataUserHoursSession } = useGlobalContext();
   const [data] = useState(dataUserHoursSession);
-  console.log(data);
+  // console.log(data);
 
   // prende tutte le ore lavorate e le somma
   const totalHours = data?.reduce((total, user) => total + (user.oreLavorate || 0), 0) || 0;
 
+  console.log(totalHours);
+  
+
   // divi le mance per le ore lavorate
   const calculateTipPercentage = (totalHours: number, oreLavorate: number) => {
     if(totalHours === 0 ) return 0
-    return oreLavorate / totalHours;
+    return totalHours / oreLavorate;
   };
 
   const tipPerUser = data?.map((user) => {
     // controlla che use.oreLavorate non sia undefined
     const oreLavorate = user.oreLavorate || 0
     const percentage = calculateTipPercentage(oreLavorate, totalHours);
+
+    console.log(percentage);
+    
     return {
       ...user,
       mancia: percentage * manciaTotale,
