ff33c54e6fd89c33bd08d82b3887dcd163081fb9
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-09-04 20:23:39 +0200

    refactor: :mute: elimina logs

--- src/components/CalcolatoreOre.tsx
+++ src/components/CalcolatoreOre.tsx
@@ -3,32 +3,28 @@ import UserGrid from "./UserGrid";
 import { useState } from "react";
 
 type calcolatoreOreProps = {
-  manciaTotale:number,
-}
-const CalcolatoreOre:React.FC<calcolatoreOreProps> = ({manciaTotale}) => {
+  manciaTotale: number;
+};
+const CalcolatoreOre: React.FC<calcolatoreOreProps> = ({ manciaTotale }) => {
   const { dataUserHoursSession } = useGlobalContext();
   const [data] = useState(dataUserHoursSession);
   // console.log(data);
 
   // prende tutte le ore lavorate e le somma
-  const totalHours = data?.reduce((total, user) => total + (user.oreLavorate || 0), 0) || 0;
-
-  console.log(totalHours);
-  
+  const totalHours =
+    data?.reduce((total, user) => total + (user.oreLavorate || 0), 0) || 0;
 
   // divi le mance per le ore lavorate
   const calculateTipPercentage = (totalHours: number, oreLavorate: number) => {
-    if(totalHours === 0 ) return 0
+    if (totalHours === 0) return 0;
     return totalHours / oreLavorate;
   };
 
   const tipPerUser = data?.map((user) => {
     // controlla che use.oreLavorate non sia undefined
-    const oreLavorate = user.oreLavorate || 0
+    const oreLavorate = user.oreLavorate || 0;
     const percentage = calculateTipPercentage(oreLavorate, totalHours);
 
-    console.log(percentage);
-    
     return {
       ...user,
       mancia: percentage * manciaTotale,
