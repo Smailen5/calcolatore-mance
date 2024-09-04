f3ca6c4e49a118318794af0d09b119b120124b30
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-09-04 17:34:54 +0200

    feat: :sparkles: funzione calcola mance
    aggiunge la funzione che calcola la percentuale e la quantita di mancia per ogni persona

--- src/components/CalcolatoreOre.tsx
+++ src/components/CalcolatoreOre.tsx
@@ -1,12 +1,38 @@
 import { useGlobalContext } from "@/utils/context";
 import UserGrid from "./UserGrid";
-const CalcolatoreOre = () => {
+import { useState } from "react";
+
+type calcolatoreOreProps = {
+  manciaTotale:number,
+}
+const CalcolatoreOre:React.FC<calcolatoreOreProps> = ({manciaTotale}) => {
   const { dataUserHoursSession } = useGlobalContext();
-  console.log(dataUserHoursSession);
+  const [data] = useState(dataUserHoursSession);
+  console.log(data);
+
+  // prende tutte le ore lavorate e le somma
+  const totalHours = data?.reduce((total, user) => total + (user.oreLavorate || 0), 0) || 0;
+
+  // divi le mance per le ore lavorate
+  const calculateTipPercentage = (totalHours: number, oreLavorate: number) => {
+    if(totalHours === 0 ) return 0
+    return oreLavorate / totalHours;
+  };
+
+  const tipPerUser = data?.map((user) => {
+    // controlla che use.oreLavorate non sia undefined
+    const oreLavorate = user.oreLavorate || 0
+    const percentage = calculateTipPercentage(oreLavorate, totalHours);
+    return {
+      ...user,
+      mancia: percentage * manciaTotale,
+    };
+  });
+
   return (
     <>
       <div>CalcolatoreOre</div>
-      <UserGrid data={dataUserHoursSession} noButton isHours/>
+      <UserGrid data={tipPerUser} noButton isHours />
     </>
   );
 };
