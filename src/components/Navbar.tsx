e4878d1cefcc346e203929381178fcbcf7d87982
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-09-04 21:18:51 +0200

    refactor: :lipstick: navbar fixed
    navbar fissata in alto

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import Menu from "./Menu";
 
 const Navbar = () => {
   return (
-    <nav className="flex justify-between bg-secondary p-4">
+    <nav className="flex justify-between bg-secondary p-4 fixed top-0 w-full">
       <h1>Calcolatore Mance</h1>
       {/* aggiungi qui il componente menu per navigare fra le pagine */}
       <Menu />
