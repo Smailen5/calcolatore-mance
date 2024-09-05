ca8cccd43432d8b26f8b3e5a13b727ba82c23740
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-09-05 19:07:06 +0200

    refactor: :lipstick: aggiunge icone
    aggiunge icone da react icons

--- src/components/sideMenu.tsx
+++ src/components/sideMenu.tsx
@@ -1,4 +1,9 @@
+import { Cross1Icon } from "@radix-ui/react-icons";
 import { useState } from "react";
+import { FaClock, FaUser } from "react-icons/fa";
+import { IoIosHome } from "react-icons/io";
+import { IoMenu } from "react-icons/io5";
+import { SiGoogledocs } from "react-icons/si";
 import { Link } from "react-router-dom";
 import { Button } from "./ui/button";
 
@@ -10,20 +15,38 @@ const SideMenu = () => {
   };
   return (
     <>
-      <div onClick={handleClick} className="cursor-pointer">Menu</div>
+      <div onClick={handleClick} className="cursor-pointer">
+        <IoMenu className="size-6" />
+      </div>
       {isOpen && (
         <div className="absolute left-0 top-0 h-screen w-full bg-white text-3xl">
           <div className="relative flex flex-col items-center justify-center">
-            <Button onClick={handleClick} className="absolute right-4 top-4">
-              Chiudi
+            <Button
+              variant={"ghost"}
+              onClick={handleClick}
+              className="absolute right-4 top-4"
+            >
+              <Cross1Icon className="size-6" />
             </Button>
 
-            <ul className="flex h-screen flex-col justify-center gap-4">
-              <Link to="/">Home</Link>
-              <Link to="/utenti">Aggiungi utenti</Link>
-              <Link to="/aggiungi-ore">Aggiungi ore</Link>
-              <Link to="">Tutorial</Link>
-            </ul>
+            <div className="flex h-screen flex-col justify-center gap-4">
+              <Link to="/" className="flex items-center gap-4">
+                <IoIosHome />
+                Home
+              </Link>
+              <Link to="/utenti" className="flex items-center gap-4">
+                <FaUser />
+                Aggiungi utenti
+              </Link>
+              <Link to="/aggiungi-ore" className="flex items-center gap-4">
+                <FaClock />
+                Aggiungi ore
+              </Link>
+              <Link to="" className="flex items-center gap-4">
+                <SiGoogledocs />
+                Tutorial
+              </Link>
+            </div>
           </div>
         </div>
       )}
