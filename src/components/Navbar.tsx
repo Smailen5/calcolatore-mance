794b9cf6ef2f3f4d689d0a880ea4b1c17e10ad72
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-09-05 18:14:50 +0200

    refactor: :speech_balloon: modifica titolo
    modifica titolo nella navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import Menu from "./Menu";
 const Navbar = () => {
   return (
     <nav className="flex justify-between bg-secondary p-4 fixed top-0 w-full">
-      <h1>Calcolatore Mance</h1>
+      <h1>Dividi Mance</h1>
       {/* aggiungi qui il componente menu per navigare fra le pagine */}
       <Menu />
     </nav>
