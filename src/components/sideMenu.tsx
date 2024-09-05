da42a8c8e5072a9497d5edac8ed687c0e3623526
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-09-05 18:47:46 +0200

    feat: :sparkles: crea componente
    crea il componente sideMenu

---
+++ src/components/sideMenu.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { Button } from "./ui/button";
+
+const SideMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+  return (
+    <>
+      <div onClick={handleClick}>Menu</div>
+      {isOpen && (
+        <div className="absolute left-0 top-0 h-screen bg-white w-full text-3xl">
+          <div className="relative flex flex-col items-center justify-center">
+            <Button onClick={handleClick} className="absolute right-4 top-4">
+              Chiudi
+            </Button>
+
+            <ul className="flex h-screen flex-col justify-center gap-4">
+              <Link to="/">Home</Link>
+              <Link to="/utenti">Aggiungi utenti</Link>
+              <Link to="/aggiungi-ore">Aggiungi ore</Link>
+              <Link to="">Tutorial</Link>
+            </ul>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default SideMenu;
