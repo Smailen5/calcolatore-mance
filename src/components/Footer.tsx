bb12678d6319a0106e1c4a8deda6d3131eb2c6b4
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-09-06 13:49:48 +0200

    refactor: :lipstick: posiziona footer
    posiziona footer fixed in fondo alla pagina

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="flex justify-center bg-secondary p-4">
+    <footer className="flex justify-center bg-secondary p-4 fixed bottom-0 w-full">
       <h3>Sviluppato con ❤ da Smailen</h3>
     </footer>
   );
