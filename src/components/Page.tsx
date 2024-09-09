2be43d5c48fb7c45aba91c5e8d0d54664b7540ba
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-09-09 15:06:34 +0200

    refactor: :lipstick: aggiorna padding
    aggiorna padding per tutte le pagine

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -7,7 +7,7 @@ type PageProps = {
 
 const Page: React.FC<PageProps> = ({ children, className }) => {
   return (
-    <main className={cn("flex min-h-[100vh] flex-col justify-center gap-8 px-4 pb-8 pt-24", className)}>
+    <main className={cn("flex min-h-[100vh] flex-col justify-center gap-8 px-4 pb-28 pt-24", className)}>
       {children}
     </main>
   );
