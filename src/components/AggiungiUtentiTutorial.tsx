53fb27433803acee1367a83cff7fedf02bcaaeeb
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-09-09 16:17:56 +0200

    refactor: :speech_balloon: aggiorna testo
    corregge e aggiorna il testo della sezione

--- src/components/AggiungiUtentiTutorial.tsx
+++ src/components/AggiungiUtentiTutorial.tsx
@@ -14,27 +14,28 @@ function AggiungiUtentiTutorial() {
             richiesti come: nome dipendente, anni di lavoro e tipo di contratto
             tra stagionale e a chiamata.*
           </p>
-          <p>
-            Dopo aver inserito tutti i parametri clicca sul bottone "Salva" alla
-            fine del form, l'utente sara subito visibile e verra salvato
-            internamente al tuo dispositivo dopo un breve tempo di attesa.
-          </p>
           <p>Qui sotto puoi vedere una immagine esplicativa.</p>
           <img
             src={formCompleto}
             alt="pagina aggiungi utenti con form completato"
           />
           <p>
-            Da questa pagina e possibile eliminare eliminare anche gli utenti
-            non piu in forza, per eliminarli e sufficiente cliccare sul bottone
-            con l'icona "X".
+            Dopo aver inserito tutti i parametri clicca sul bottone "Salva" alla
+            fine del form, l'utente sara subito visibile e verra salvato
+            internamente al tuo dispositivo dopo un breve tempo di attesa.
+          </p>
+          <p>
+            Da questa pagina e possibile eliminare anche gli utenti non piu in
+            forza, per eliminarli e sufficiente cliccare sul bottone con l'icona
+            "X".
           </p>
           <img src={utenteAggiunto} alt="" />
           <p className="text-sm italic">
             *Tutti i parametri sono obbligatori anche se per il momento
-            utilizzeremo solo il nome per dividere le mance. Prevediamo in
-            futuro di implementare nuove funzionalita e permettere la divisione
-            delle mance secondo tipo di contratto e anzianita di anni di lavoro.
+            utilizzeremo solo il nome e la quantita di ore lavorate per dividere
+            le mance. Prevediamo in futuro di implementare nuove funzionalita e
+            permettere la divisione delle mance secondo tipo di contratto e
+            anzianita di anni di lavoro.
           </p>
         </article>
       </div>
