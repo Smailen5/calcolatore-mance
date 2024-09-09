6a36c8a55393874a10cc9442133be720e6f754c4
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-09-09 16:10:02 +0200

    refactor: :speech_balloon: aggiunge testo
    aggiunge il testo per la sezione Aggiungi/rimuovi utenti con un minimo di stile

--- src/components/AggiungiUtentiTutorial.tsx
+++ src/components/AggiungiUtentiTutorial.tsx
@@ -1,16 +1,45 @@
-import aggiungiUtenti from "../assets/aggiungi-utenti.png"
+import formCompleto from "../assets/screenshot/aggiungi-utente-form-completo.jpeg";
+import PageTutorial from "./PageTutorial";
+import utenteAggiunto from "../assets/screenshot/utente-aggiunto.jpeg";
 
 function AggiungiUtentiTutorial() {
-    return (
-      <section id="aggiungi-utenti">
+  return (
+    <PageTutorial>
+      <div id="aggiungi-utenti">
         {/* Prima sezione come si aggiungo gli utenti e il perche di tutti i parametri */}
-  
-        <p>
-          scrivi qui come usare l'applicazione spiegando tutte le funzionalita
-        </p>
-        <img src={aggiungiUtenti} alt="" />
-      </section>
-    );
-  }
+        <h2 className="my-8 text-xl font-semibold">Aggiungi/rimuovi utenti</h2>
+        <article className="space-y-4">
+          <p>
+            Per aggiungere un nuovo utente devi aggiungere tutti i parametri
+            richiesti come: nome dipendente, anni di lavoro e tipo di contratto
+            tra stagionale e a chiamata.*
+          </p>
+          <p>
+            Dopo aver inserito tutti i parametri clicca sul bottone "Salva" alla
+            fine del form, l'utente sara subito visibile e verra salvato
+            internamente al tuo dispositivo dopo un breve tempo di attesa.
+          </p>
+          <p>Qui sotto puoi vedere una immagine esplicativa.</p>
+          <img
+            src={formCompleto}
+            alt="pagina aggiungi utenti con form completato"
+          />
+          <p>
+            Da questa pagina e possibile eliminare eliminare anche gli utenti
+            non piu in forza, per eliminarli e sufficiente cliccare sul bottone
+            con l'icona "X".
+          </p>
+          <img src={utenteAggiunto} alt="" />
+          <p className="text-sm italic">
+            *Tutti i parametri sono obbligatori anche se per il momento
+            utilizzeremo solo il nome per dividere le mance. Prevediamo in
+            futuro di implementare nuove funzionalita e permettere la divisione
+            delle mance secondo tipo di contratto e anzianita di anni di lavoro.
+          </p>
+        </article>
+      </div>
+    </PageTutorial>
+  );
+}
 
-  export default AggiungiUtentiTutorial
+export default AggiungiUtentiTutorial;
